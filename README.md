745ac166219dcd31e6d2da9008fc3639e8313a15
ElenaAnnibali <77542450+ElenaAnnibali@users.noreply.github.com>
2022-05-03 17:27:07 +0200

    Print random hex color

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-abc
+# Node.js Random Color Generator
+
+```
+$ node index.js
+
+
+```
+
+- without any user input: print random color with hex code
+- with user input hue: print matching random color
+- with user input luminosity: print matching random color
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('string');
+import boxen from 'boxen';
+import chalk from 'chalk';
+import uniqolor from 'uniqolor';
+
+const hexColor = uniqolor.random().color;
+
+const getColor = chalk.hex(uniqolor.random().color);
+
+//const box = boxen(getColor);
+
+console.log(getColor(hexColor));
